497b40d72e68a1616e71ff6b0c60ed1f12669ee5
Samuel Longshak <difrentecho@gmail.com>
2019-06-07 19:39:22 +0100

    Modified models

--- flights/models.py
+++ flights/models.py
@@ -3,8 +3,20 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 # Create your models here.
+
+
+class Airline(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='To Location'
+    )
+    flight_number = models.CharField(
+        max_length=100,
+        verbose_name='To Location'
+    )
+
+
 class Flight(models.Model):
     """
      Flight model definition
@@ -23,18 +35,7 @@ class Flight(models.Model):
     arrival_time = models.DateTimeField(
         verbose_name='Arrival Time'
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        verbose_name='Created At'
-    )
-    flight_nunber = models.CharField(
-        max_length=100,
-        verbose_name='Flight Number'
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True,
-        verbose_name='Updated At'
-    )
+    airline = models.ForeignKey(Airline, on_delete=models.CASCADE)
     no_of_seats = models.IntegerField()
 
     price = models.IntegerField(default=0)
@@ -43,6 +44,14 @@ class Flight(models.Model):
         User,
         through='Booking',
         through_fields=('flight', 'user')
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Created At'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Updated At'
     )
 
     class Meta:
